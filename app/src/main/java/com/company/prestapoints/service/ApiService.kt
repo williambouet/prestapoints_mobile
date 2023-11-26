dbed0b23790d5f70b8d0d4ae522b9c0481b5b4eb
JauffritCharly <jauffrit.charly@hotmail.com>
2023-11-26 19:12:48 +0100

    formulaire connexion fonctionnel

--- app/src/main/java/com/company/prestapoints/service/ApiService.kt
+++ app/src/main/java/com/company/prestapoints/service/ApiService.kt
@@ -1,10 +1,14 @@
 package com.company.prestapoints.service
 
+import com.company.prestapoints.model.ApiResponse
 import com.company.prestapoints.model.Categories
 import com.company.prestapoints.model.Category
+import com.company.prestapoints.model.UserSignIn
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface ApiService {
     @GET("endpoint")
@@ -15,4 +19,7 @@ interface ApiService {
 
     @GET("categories")
     fun getCategories(): Call<List<Category>>
+
+    @POST("public/sign-in")
+    fun signUp(@Body request: UserSignIn): Call<ApiResponse>
 }
